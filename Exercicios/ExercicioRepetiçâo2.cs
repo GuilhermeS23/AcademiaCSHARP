e5ad514d11f8b7d8a984e7506798049981df04bf
GuilhermeS23 <146769910+GuilhermeS23@users.noreply.github.com>
2023-10-19 14:19:28 -0300

    Add files via upload

---
+++ Exercicios/ExercicioRepetiçâo2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maior = int.MinValue;
+
+        for (int i = 0; i < 10; i++) 
+        {
+
+            Console.Write($"digite o {i + 1}º Número: ");
+            int numero = int.Parse( Console.ReadLine() );
+
+            if(numero > maior)
+            {
+                maior = numero;
+            }
+
+        }
+
+        Console.WriteLine($"O maior número é {maior:F2} ");
+
+    }
+
+}
