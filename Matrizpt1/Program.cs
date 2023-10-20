0eb43924bebeb3cb0be51d407867fecc392c86cc
GuilhermeS23 <146769910+GuilhermeS23@users.noreply.github.com>
2023-10-20 15:57:13 -0300

    Add files via upload

---
+++ Matrizpt1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] matriz = new int[3, 3];
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.WriteLine("Informe um valor " +
+            "para M[" + i + "][" + j + "]:");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.Write("[" + matriz[i, j] + "]");
+    }
+
+    Console.WriteLine();
+}
+
+
+Console.WriteLine();
+
+for (int i = matriz.GetLength(0) - 1; i >= 0; i--)
+{
+    Console.WriteLine("[" + matriz[i, ((matriz.GetLength(0) - 1) - i)] + "]");
+}
