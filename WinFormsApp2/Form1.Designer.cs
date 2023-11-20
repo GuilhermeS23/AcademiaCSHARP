1f88a9226c54bf4aaf3605eca5015a6bce865fa8
GuilhermeS23 <146769910+GuilhermeS23@users.noreply.github.com>
2023-11-19 22:33:46 -0300

    Add files via upload

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cadastrarPessoa = new Button();
+            gerarAmigoSecreto = new Button();
+            SuspendLayout();
+            // 
+            // cadastrarPessoa
+            // 
+            cadastrarPessoa.Location = new Point(211, 83);
+            cadastrarPessoa.Name = "cadastrarPessoa";
+            cadastrarPessoa.Size = new Size(75, 23);
+            cadastrarPessoa.TabIndex = 0;
+            cadastrarPessoa.Text = "Adicionar";
+            cadastrarPessoa.UseVisualStyleBackColor = true;
+            cadastrarPessoa.Click += cadastrarPessoa_Click;
+            // 
+            // gerarAmigoSecreto
+            // 
+            gerarAmigoSecreto.Location = new Point(487, 83);
+            gerarAmigoSecreto.Name = "gerarAmigoSecreto";
+            gerarAmigoSecreto.Size = new Size(75, 23);
+            gerarAmigoSecreto.TabIndex = 1;
+            gerarAmigoSecreto.Text = "Gerar Duplas";
+            gerarAmigoSecreto.UseVisualStyleBackColor = true;
+            gerarAmigoSecreto.Click += gerarAmigoSecreto_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(gerarAmigoSecreto);
+            Controls.Add(cadastrarPessoa);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button cadastrarPessoa;
+        private Button gerarAmigoSecreto;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cadastrarPessoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gerarAmigoSecreto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
